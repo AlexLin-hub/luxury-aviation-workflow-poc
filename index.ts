2ef3eec1c0081c0df2d74befb421713bb6cd1541
Alex Lin <unclelin2020@gmail.com>
2026-04-05 16:31:25 +0800

    feat: install gemini genAI

--- index.ts
+++ index.ts
@@ -1,19 +1,29 @@
 import fastify, { type FastifyInstance } from "fastify";
 import { type TypeBoxTypeProvider } from "@fastify/type-provider-typebox";
 import { Type } from "typebox"
+import { GenerateContentResponse, GoogleGenAI, type GenerateContentParameters } from "@google/genai";
 
 const server = fastify().withTypeProvider<TypeBoxTypeProvider>();
+const ai = new GoogleGenAI({})
+
 const bookingSchema = Type.Object({
     message: Type.String(),
     email: Type.String({ format: "email" }),
 });
 
-// type Booking = Type.Static<typeof bookingSchema>;
+type Booking = Type.Static<typeof bookingSchema>;
 
-function handleBooking(fastify: FastifyInstance) {
+async function askGemini(contents: GenerateContentParameters['contents']): Promise<GenerateContentResponse['text']> {
+    const response = await ai.models.generateContent({ model: "gemini-2.5-flash", contents })
+    const { text } = response
+    return text
+}
+
+async function handleBooking(fastify: FastifyInstance) {
     fastify.post("/booking", { schema: { body: bookingSchema } }, async (request, reply) => {
-        // const body = request.body as Booking;
-        reply.status(200).send({ message: "Booking received." })
+        const body = request.body as Booking
+        const { message } = body
+        reply.status(200).send({ message: await askGemini(message) })
     });
 }
 
